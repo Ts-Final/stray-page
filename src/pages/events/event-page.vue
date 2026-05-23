b03f6fe400089c94c107a2c287e37b5ac664eda3
Ts-Final <139440186+Ts-Final@users.noreply.github.com>
2026-05-23 22:27:27 +0800

    added reactivity to eventPage

--- src/pages/events/event-page.vue
+++ src/pages/events/event-page.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
 import Ev1 from "./event-pages/ev1.vue"
-import {useRoute} from "vue-router"
-import type {Component} from "vue"
+import {useRouter} from "vue-router"
+import {type Component, computed} from "vue"
 import Ev2 from "./event-pages/ev2.vue"
 
-const route = useRoute()
-const id = parseInt(route.params.id as string ?? "0")
+const route = useRouter()
+const id = computed(() => parseInt(route.currentRoute.value.params.id as string ?? "0"))
 const components: Record<number, Component> = {
   1: Ev1,
   2: Ev2
@@ -15,9 +15,9 @@ const components: Record<number, Component> = {
 <template>
   <div>
     <component :is="components[id]" v-if="components[id]"/>
-  <div v-else class="contain-90">
-    Nothing Here...
-  </div>
+    <div v-else class="contain-90">
+      Nothing Here...
+    </div>
   </div>
 </template>
 
